874f46fcd0af4a5c2949d51b7d313c7bdddc6f08
Blessing-Chuks <blechukwudi7@gmail.com>
2024-12-19 10:09:15 +0100

    edit error and debug

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import "../assets/css/Hero.css";
+import "../assets/css/hero.css";
 
 const Hero = () => {
   return (
